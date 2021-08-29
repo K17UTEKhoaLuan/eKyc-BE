9df9dca116cb57c223a904cbc1babb1b6d0ddc11
Caoquyen1913 <caoquyen1913@gmail.com>
2021-08-29 18:48:13 +0700

    change width height

--- processImage.py
+++ processImage.py
@@ -8,7 +8,7 @@ def open_image(image_name):
     # img = cv2.imread(image_name)
     # if(len(img)>0):
     #     return True, img
-    # else: 
+    # else:
     #     return False, ""
     image = open(image_name, 'rb')
 
@@ -26,12 +26,12 @@ def cropImageIdentifyNumber(img):
 def cropIdentity(img_name, size):
     img = cv2.imread(img_name)
     print(size.name)
-    coordinate_up_left = (size.imageHeight - size.identityHeight)/2 
-    coordinate_left_up = (size.imageWidth - size.identityWidth)/2
+    coordinate_up_left = (size.imageWidth - size.identityWidth)/2
+    coordinate_left_up = (size.imageHeight - size.identityHeight)/2
     y = int(coordinate_up_left)
-    h = int(coordinate_up_left + size.identityHeight)
+    h = int(coordinate_up_left + size.identityWidth)
     x = int(coordinate_left_up)
-    w = int(coordinate_left_up + size.identityWidth)
+    w = int(coordinate_left_up + size.identityHeight)
     crop = img[y:h, x:w]
     cv2.imwrite("frontside/{}_frontside.jpg".format(size.name), crop)
     return crop

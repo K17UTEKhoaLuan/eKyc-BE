0d51a1e4672ca3ee5e94facb99d84633642bea7d
Caoquyen1913 <caoquyen1913@gmail.com>
2021-09-13 13:37:09 +0700

    print string

--- src/api/image/controller.py
+++ src/api/image/controller.py
@@ -9,6 +9,7 @@ router = APIRouter()
 
 @router.post("/crop", response_model=Crop_Model)
 def crop_cmnd(data: Image):
+    print("string: ", data.image)
     img = convert.convert_base64_to_image(data.image)
     croped_image = processImage.cropIdentity(img=img, item=data)
     base64_string = convert.convert_image_to_base64(croped_image)

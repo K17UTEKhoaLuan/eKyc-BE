eabf76267bb56f77698f64caed22b369a5ddb62c
Caoquyen1913 <caoquyen1913@gmail.com>
2021-09-13 13:11:08 +0700

    change

--- src/process/processImage.py
+++ src/process/processImage.py
@@ -27,10 +27,10 @@ def cropImageIdentifyNumber(img):
 def cropIdentity(img, item):
     coordinate_up_left = (item.imageWidth - item.identityWidth)/2
     coordinate_left_up = (item.imageHeight - item.identityHeight)/2
-    print("coordinate_up_left", coordinate_up_left+"-->" +
-          int(coordinate_up_left + item.identityWidth))
-    print("coordinate_left_up", coordinate_left_up+"-->" +
-          int(coordinate_left_up + item.identityHeight))
+    # print("coordinate_up_left", coordinate_up_left+"-->" +
+    #       int(coordinate_up_left + item.identityWidth))
+    # print("coordinate_left_up", coordinate_left_up+"-->" +
+    #       int(coordinate_left_up + item.identityHeight))
 
     y = int(coordinate_up_left)+50
     h = int(coordinate_up_left + item.identityWidth)-50
